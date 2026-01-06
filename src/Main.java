a0d8961409d77d155f298a46ab53245cc32c532a
LukasDD82 <lukas.simek.de@gmail.com>
2026-01-06 21:21:44 +0100

    Initial commit + List

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+import java.util.List;
+
+class Main{
+
+    private final List<String> resistorColours = List.of(
+            "black", "brown", "red", "orange", "yellow",
+            "green", "blue", "violet", "grey", "white"
+    );
+}
