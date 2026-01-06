aff1b3eb91fcede37f4da6979da409db1c51032a
LukasDD82 <lukas.simek.de@gmail.com>
2026-01-06 21:25:59 +0100

    Base

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,16 @@
 import java.util.List;
 
-class Main{
+class Main {
 
     private final List<String> resistorColours = List.of(
             "black", "brown", "red", "orange", "yellow",
             "green", "blue", "violet", "grey", "white"
     );
+
+    String label(String[] colors) {
+        int grund = resistorColours.indexOf(colors[0]) * 10 + resistorColours.indexOf(colors[1]);
+        int base = resistorColours.indexOf(colors[2]);
+        long multiply = (long) Math.pow(10, base);
+
+    }
 }
