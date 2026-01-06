228e865f51b03ea07a03dadd9e38ea2f03fe6043
LukasDD82 <lukas.simek.de@gmail.com>
2026-01-06 21:41:32 +0100

    Final version

--- src/Main.java
+++ src/Main.java
@@ -8,9 +8,44 @@ class Main {
     );
 
     String label(String[] colors) {
-        int grund = resistorColours.indexOf(colors[0]) * 10 + resistorColours.indexOf(colors[1]);
+        int basisFormula = resistorColours.indexOf(colors[0]) * 10 + resistorColours.indexOf(colors[1]);
         int base = resistorColours.indexOf(colors[2]);
         long multiply = (long) Math.pow(10, base);
 
+        if (colors[2].equals ("black")) {
+            return  basisFormula * multiply + " " + "ohms";
+        }
+        else if (colors[2].equals ("brown")) {
+            return  basisFormula * multiply + " " +  "ohms";
+        }
+        else if (colors[2].equals ("red")) {
+            return   basisFormula * multiply / 1000 + " " +  "kiloohms";
+        }
+        else if (colors[2].equals ("orange")) {
+            return   basisFormula * multiply / 1000 + " " +  "kiloohms";
+        }
+        else if (colors[2].equals ("yellow")) {
+            return  basisFormula * multiply / 1000 + " " +  "kiloohms";
+        }
+        else if (colors[2].equals ("green")) {
+            return basisFormula + multiply /1000 + " " +  "kiloohms";
+        }
+        else if (colors[2].equals ("blue")) {
+            return basisFormula * multiply  /1000000 + " " +  "megaohms";
+        }
+        else if (colors[2].equals ("violet")) {
+            return basisFormula * multiply /1000000 + " " +  "megaohms";
+        }
+        else if (colors[2].equals ("grey")) {
+            return basisFormula * multiply / 1000000 + " " +  "megaohms";
+        }
+        else if (colors[2].equals ("white")) {
+            return basisFormula * multiply/1000000000 + " " +  "gigaohms";
+        }
+        else {return "incorect color";
+        }
     }
 }
+
+
+
